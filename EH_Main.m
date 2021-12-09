f895e0c96236cd27dfc378c6ca04190e75b1aa85
seanrex90 <79457889+seanrex90@users.noreply.github.com>
2021-12-09 18:14:16 +0530

    Update EH_Main.m

--- EH_Main.m
+++ EH_Main.m
@@ -21,6 +21,6 @@ time = 0 ;
 
 for n_nodes = 1 : 10
 
-throughput = EH_Random_Access(n_nodes,buff_cap,batt_cap,max_retx);
+throughput = EH_Random_Access("n_nodes,buff_cap,batt_cap,max_retx");
 
 end
